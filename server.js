aafc0b07b06eae05da4da3023581449341568c4d
jayfortier79 <jfortier05@yahoo.com>
2023-08-29 09:58:22 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express")
+
+const app = express()
+
+app.get('/greeting', (request, response) => {
+    response.send("<h1>Whats's up<h1>" + request.params.name)
+})
+
+app.get('/greeting/:name', (request, response) =>{
+
+    response.send("<h1>What's up</h1>")
+})
+
+app.listen(3000, ()=>{
+    console.log("Greets has been initiated")
+})
